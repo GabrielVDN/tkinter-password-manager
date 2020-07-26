844683968893b683f630e5ee38d0f20997ad0c27
sebastianvdn <gabriel52.van.den.noortgaete@gmail.com>
2020-07-26 19:36:20 +0200

    Placed the tree

--- frames/list.py
+++ frames/list.py
@@ -10,25 +10,37 @@ class List(ttk.Frame):
         # Center your Frame in the middle-top.
         self.columnconfigure(0, weight=1)
 
-        label_1 = ttk.Label(
-            self, text="Double Click To Copy Password"
-        )
-        label_1.grid(row=0, column=0)
-        
+        tframe1=tk.Frame(self)
+        tframe1.pack(side='top')
+        headings = ["Service", "Username"]
+
+        # Adding the Treeview
+        tk.Label(tframe1, text="Double Click to copy password",
+                bd=2).pack(side='left', padx=10,pady=10)
         btn_back = ttk.Button(
-            self,
+            tframe1,
             text="🔙",
             command=lambda: controller.show_frame("Home"),
             cursor="hand2",
             width=3
         )
-        btn_back.grid(row=0, column=1, sticky="E")
-
-        tree = ttk.Treeview(
-            self,
-        )
-        tree.grid(row=1, columnspan=2)
+        btn_back.pack(side='left', padx=10,pady=10)
 
 
-        for child in self.winfo_children():
-            child.grid_configure(padx=8, pady=8)
+        tframe2=tk.Frame(self)
+        tframe2.pack(side='bottom')
+
+        scroll = ttk.Scrollbar(tframe2, orient='vertical', takefocus=True)
+        self.tree = ttk.Treeview(tframe2, columns=headings, show="headings")
+        scroll.config(command=self.tree.yview)
+        self.tree.configure(yscroll=scroll.set)
+
+        scroll.pack(side='right', fill='y')
+        self.tree.pack(side='left', fill='both', expand=1)
+
+        # Adding headings to the columns and resp. cmd's
+        for heading in headings:
+            self.tree.heading(
+                heading, text=heading,
+                command=lambda c=heading: self.sortby(self.tree, c, 0))
+            self.tree.column(heading, width=400)
